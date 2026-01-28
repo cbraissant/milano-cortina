8abda6105226d78034007d8f0ad7d32ab3fd9733
Chris Braissant <chrisbraissant@gmail.com>
2026-01-28 14:08:29 +0100

    update layout and add next/previous actions

--- script.js
+++ script.js
@@ -2,32 +2,85 @@ const scenes = document.querySelectorAll('.scene');
 let index = 0;
 let paused = false;
 
-document.addEventListener('keydown', e => {
-  const scene = scenes[index];
-  const video = scene.querySelector('.video');
+const labelInsetSeconds = 2;
 
-  if (e.code === 'Space') {
-    e.preventDefault();
-    paused = !paused;
-    paused ? video.pause() : video.play();
-  }
+
+/* Keyboard commands */
+document.addEventListener('keydown', e => {
+	const scene = scenes[index];
+	const video = scene.querySelector('.video');
+	
+	if (e.code === 'Space') {
+		e.preventDefault();
+		pauseScene(video);
+	}
+	
+	if (e.code === 'ArrowRight') {
+		e.preventDefault();
+		playNext();
+	}
+	
+	if (e.code === 'ArrowLeft') {
+		e.preventDefault();
+		playPrevious();
+	}
+	
 });
 
+
+function pauseScene(video) {
+	paused = !paused;
+	paused ? video.pause() : video.play();
+}
+
+
+function playNext() {
+	index = (index + 1) % scenes.length;
+	playScene(index);
+}
+
+
+function playPrevious() {
+	index = (index - 1 + scenes.length) % scenes.length;
+	playScene(index);
+}
+
+
 function playScene(i) {
-  scenes.forEach(s => s.classList.remove('active'));
+	scenes.forEach(s => {
+		s.classList.remove('active');
+	});
+	
+	const scene = scenes[i];
+	const video = scene.querySelector('.video');
+	const label = scene.querySelector('.label');
+	
+	scene.classList.add('active');
+	
+	// cleanup
+	video.onended = null;
+	video.ontimeupdate = null;
+	
+	video.currentTime = 0;
+	if (!paused) video.play();
+	
+	// logic to display the label after a certain time
+	video.ontimeupdate = () => {
+		const time = video.currentTime;
+		const duration = video.duration;
+		
+		if (!duration) return;
+		
+		// display the label after 1sec 
+		if (time >= labelInsetSeconds && time < duration - labelInsetSeconds) {
+			label.classList.add('active');
+		} else {
+			label.classList.remove('active');
+			
+		}
+	};
 
-  const scene = scenes[i];
-  const video = scene.querySelector('.video');
-
-  scene.classList.add('active');
-  video.onended = null;
-  video.currentTime = 0;
-  video.play();
-
-  video.onended = () => {
-    index = (index + 1) % scenes.length;
-    playScene(index);
-  };
+	video.onended = playNext;
 }
 
 playScene(0);
